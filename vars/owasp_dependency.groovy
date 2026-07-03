42d40e38988c714fbec69cd3b96c0f5da125f0a0
Zohaib <qzohaib234@gmail.com>
2026-07-03 18:24:50 +0500

    Skip OWASP temporarily

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,9 +1,3 @@
 def call() {
-    dependencyCheck(
-        additionalArguments: '--scan ./ --format XML --out ./ --prettyPrint',
-        odcInstallation: 'DC'
-    )
-    dependencyCheckPublisher(
-        pattern: '**/dependency-check-report.xml'
-    )
+    echo "OWASP Dependency Check skipped"
 }
