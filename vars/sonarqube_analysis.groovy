9d5c46cdeaa06cc7f7669ffe741ffe28bd7ac0d8
Zohaib <qzohaib234@gmail.com>
2026-07-03 23:12:47 +0500

    Added sonarcloud organization key

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,11 +1,11 @@
 def call(String sonarTool, String projectName, String projectKey) {
     withSonarQubeEnv(sonarTool) {
-        // SONAR_HOME is set in the Jenkinsfile via:
-        // environment { SONAR_HOME = tool "Sonar" }
         sh """
             \$SONAR_HOME/bin/sonar-scanner \
                 -Dsonar.projectName=${projectName} \
                 -Dsonar.projectKey=${projectKey} \
+                -Dsonar.organization=zohaibq112 \
+                -Dsonar.host.url=https://sonarcloud.io \
                 -Dsonar.sources=. \
                 -Dsonar.java.binaries=.
         """
