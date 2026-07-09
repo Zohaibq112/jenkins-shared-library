9a5b39f8cfdd3022e4e082818791c07c5eb33356
Zohaib <qzohaib234@gmail.com>
2026-07-09 23:57:25 +0500

    Retry checkout on network failure

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -1,10 +1,13 @@
 def call(String url, String branch) {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: "*/${branch}"]],
-        extensions: [
-            [$class: 'CloneOption', shallow: true, depth: 1, noTags: true, timeout: 30]
-        ],
-        userRemoteConfigs: [[url: url, credentialsId: 'Github-cred']]
-    ])
+    retry(3) {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${branch}"]],
+            extensions: [
+                [$class: 'CloneOption', shallow: true, depth: 1, noTags: true, timeout: 30],
+                [$class: 'WipeWorkspace']
+            ],
+            userRemoteConfigs: [[url: url, credentialsId: 'Github-cred']]
+        ])
+    }
 }
