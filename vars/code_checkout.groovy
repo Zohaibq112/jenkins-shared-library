ac90832a30ae05d402595f130a6519f1a0c5e508
Zohaib <qzohaib234@gmail.com>
2026-07-16 15:35:09 +0500

    fixing timeput

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -1,13 +1,16 @@
 def call(String url, String branch) {
-    retry(3) {
-        checkout([
-            $class: 'GitSCM',
-            branches: [[name: "*/${branch}"]],
-            extensions: [
-                [$class: 'CloneOption', shallow: true, depth: 1, noTags: true, timeout: 30],
-                [$class: 'WipeWorkspace']
-            ],
-            userRemoteConfigs: [[url: url, credentialsId: 'Github-cred']]
-        ])
-    }
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        extensions: [[
+            $class: 'CloneOption',
+            shallow: true,
+            depth: 1,
+            timeout: 30
+        ]],
+        userRemoteConfigs: [[
+            url: url,
+            credentialsId: 'Github-cred'
+        ]]
+    ])
 }
