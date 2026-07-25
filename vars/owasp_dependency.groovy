62079eaa20f67980123e4a8d7205111359f3cfd6
Zohaib <qzohaib234@gmail.com>
2026-07-26 01:47:05 +0500

    fixing timeoout problem

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,3 +1,16 @@
 def call() {
-    echo "OWASP Dependency Check skipped"
+    try {
+        timeout(time: 10, unit: 'MINUTES') {
+            dependencyCheck(
+                additionalArguments: '--scan ./ --format XML --out ./ --prettyPrint',
+                odcInstallation: 'DC'
+            )
+            dependencyCheckPublisher(
+                pattern: '**/dependency-check-report.xml'
+            )
+        }
+    } catch(Exception e) {
+        echo "OWASP check timed out or failed: ${e.message}"
+        echo "Continuing pipeline..."
+    }
 }
